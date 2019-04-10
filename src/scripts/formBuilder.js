e680fc088d4c7541c018457228874da38aeeba3f
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-10 14:25:22 -0500

    started work on a modular form builder

--- src/scripts/formBuilder.js
+++ src/scripts/formBuilder.js
@@ -1,3 +1,13 @@
+import htmlBuilder from "./htmlBuilder";
+
 export default {
-    
+    buildForm: (information, fieldCount, origType, origValue) => {
+        const form = htmlBuilder.elementBuilder("label", `${information}--${fieldCount}`, `${origType}`, undefined, )
+        while (fieldCount > 0){
+            fieldCount -= 1;
+            //(elementType, elementId, elementTextContent, elementValue)
+            const label = htmlBuilder.elementBuilder("label", `${information}--${fieldCount}`, `${origType}`, undefined, ) //?
+            const field = htmlBuilder.elementBuilder("label", `${information}--${fieldCount}`, `${origType}`, undefined, ) //?
+        }
+    },
 }
