c51376fe927295ef3f11d7ef472244d499af0a0e
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-10 14:59:01 -0500

    further hashed out plan for what data the form builder needs in order to function as a self-contained module

--- src/scripts/formBuilder.js
+++ src/scripts/formBuilder.js
@@ -1,13 +1,19 @@
 import htmlBuilder from "./htmlBuilder";
 
+const fieldFragment = document.createDocumentFragment()
+
 export default {
-    buildForm: (information, fieldCount, origType, origValue) => {
-        const form = htmlBuilder.elementBuilder("label", `${information}--${fieldCount}`, `${origType}`, undefined, )
-        while (fieldCount > 0){
-            fieldCount -= 1;
-            //(elementType, elementId, elementTextContent, elementValue)
-            const label = htmlBuilder.elementBuilder("label", `${information}--${fieldCount}`, `${origType}`, undefined, ) //?
-            const field = htmlBuilder.elementBuilder("label", `${information}--${fieldCount}`, `${origType}`, undefined, ) //?
+    //Title of form, Array of original keys, array of original values, array of types of fields, id from dataset.
+    buildForm: (title, keysArray, valuesArray, typesArray, id) => {
+        //(elementType, elementId, elementTextContent, elementValue)
+        //create form.
+        const form = htmlBuilder.elementBuilder("form", `${title}--${id}`, undefined, undefined, )
+        //loops through keys and builds a form and leble from the passed data.
+        for (let i = 0; keysArray.length; i+=1){
+            const label = htmlBuilder.elementBuilder("label", `${}--${}`, `${title}`, undefined, ) //?
+            const field = htmlBuilder.elementBuilder(`${}`, `${}--${}`, `${}`, undefined, ) //?
+            fieldFragment.appendChild(label);
+            fieldFragment.appendChild(field);
         }
     },
 }
