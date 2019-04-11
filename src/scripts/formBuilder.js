11e2d1e58b844e013a31e116c00a56a2f2a939e5
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-11 11:19:48 -0500

    extended the functionality of the form builder

--- src/scripts/formBuilder.js
+++ src/scripts/formBuilder.js
@@ -2,18 +2,27 @@ import htmlBuilder from "./htmlBuilder";
 
 export default {
     //Title of form, Array of original keys, array of original values, array of types of fields, id from dataset.
-    buildForm: (title, keysArray, valuesArray, typesArray, id) => {
+    buildForm: (title, keysArray, valuesArray, typesArray, id, arrayOptionsArray) => {
         //(elementType, elementId, elementTextContent, elementValue)
         //create form.
         const form = htmlBuilder.elementBuilder("fieldset", `${title}--${id}`, undefined, undefined)
         form.appendChild("legend", `legend--${title}--${id}`, `${title}:`, undefined);
         //loops through keys and builds a form and label from the passed data.
-        for (let i = 0; keysArray.length; i+=1){
+        for (let i = 0; keysArray.length; i += 1) {
             //container for the label/form pairs.
             const div = htmlBuilder.elementBuilder("div", `div--${keysArray[i]}--${id}`, undefined, undefined)
             //label and form
             const label = htmlBuilder.elementBuilder("label", `label--${keysArray[i]}--${id}`, `${keysArray[i]}`, undefined) //?
-            const field = htmlBuilder.elementBuilder(`${typesArray[i]}`, `${typesArray[i]}--${keysArray[i]}--${id}`, undefined, `${valuesArray[i]}`) //?
+            //specify type
+            let field
+            if (typesArray[i] === "textarea") {
+                field = htmlBuilder.elementBuilder(`${typesArray[i]}`, `${typesArray[i]}--${keysArray[i]}--${id}`, undefined, `${valuesArray[i]}`) //?
+            } else if (typesAray[i] === "select") {
+                field = htmlBuilder.elementBuilder(`${typesArray[i]}`, `${typesArray[i]}--${keysArray[i]}--${id}`, undefined, `${valuesArray[i]}`) //?
+            } else {
+                field = htmlBuilder.elementBuilder("input", `${typesArray[i]}--${keysArray[i]}--${id}`, undefined, `${valuesArray[i]}`) //?
+                field.setAtrribute("type", `${typesArray[i]}`);
+            }
             //append to div, append div to form.
             div.appendChild(label);
             div.appendChild(field);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,17 @@
 import formBuilder from "./formBuilder.js"
 
 //(title, keysArray, valuesArray, typesArray, id)
-keysArray = [];
-valuesArray = [];
-typesArray = [];
+const title = "Test"
+const id = 3
 
-formBuilder.buildForm(title, keysArray, valuesArray, typesArray, id)
+//select and textarea are tags on their own and not input types, but the code will understand and build the proper element. Checkbox and radio are defined each individually, but again the code is set up to understand that.
+
+keysArray = ["Test1", "Test2", "Dropdown", "Radio", "checkbox", "textarea"];
+valuesArray = ["This is test1", "This is test2", undefined, undefined,undefined, undefined];
+typesArray = ["text", "date", "select", "radio", "checkbox", "textarea"];
+dropDownOptions = ["Love", "Hate", "Indifference"]
+radioOptions = ["True", "False"]
+checkboxOptions = ["This is a test!", "This is also a test!"]
+arrayOptionsArray = [undefined, undefined, dropDownOptions, radioOptions, checkboxOptions, undefined]
+
+formBuilder.buildForm(title, keysArray, valuesArray, typesArray, id, arrayOptionsArray)
